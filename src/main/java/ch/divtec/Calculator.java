dcec2b4ddb170233811176f0d5122f50a3836a8d
Ryan Berthoud <ryan.berthoud@divtec.ch>
2026-03-27 11:11:48 +0100

    Commit pour le début de la nouvelle journée du 27.03.2026

--- src/main/java/ch/divtec/Calculator.java
+++ src/main/java/ch/divtec/Calculator.java
@@ -1,12 +1,65 @@
 package ch.divtec;
 
+import java.util.Scanner;
+
 public class Calculator {
 
-    public int add(int a, int b){
-        return a + b;
+    double add(double a, double b) {
+        throw new UnsupportedOperationException("Method not yet implemented.");
     }
 
-    public static void main (String[] args){
-        System.out.println("Hello world ! \nGood pet.");
+    double subtract(double a, double b) {
+        throw new UnsupportedOperationException("Method not yet implemented.");
+    }
+
+    double multiply(double a, double b) {
+        throw new UnsupportedOperationException("Method not yet implemented.");
+    }
+
+    double divide(double a, double b) {
+        throw new UnsupportedOperationException("Method not yet implemented.");
+    }
+
+    long factorial(double n) {
+        throw new UnsupportedOperationException("Method not yet implemented.");
+    }
+
+    public static void main(String[] args) {
+        double num1, num2 = 0.;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Allowed operators: + - * / !");
+        System.out.println("Enter the operation to compute :");
+
+        num1 = sc.nextDouble();
+        char operator = sc.next().charAt(0);
+        if (operator != '!') {
+            num2 = sc.nextDouble();
+        }
+
+        final Calculator calc = new Calculator();
+
+        double result;
+        switch (operator) {
+            case '+':
+                result = calc.add(num1, num2);
+                break;
+            case '-':
+                result = calc.subtract(num1, num2);
+                break;
+            case '*':
+                result = calc.multiply(num1, num2);
+                break;
+            case '/':
+                result = calc.divide(num1, num2);
+                break;
+            case '!':
+                result = calc.factorial(num1);
+                break;
+            default:
+                System.out.println("Invalid operator.");
+                return;
+        }
+        System.out.println("The final result: " + num1 + " " + operator + " " + num2 + " = " + result);
     }
 }
